76199886d5d89934f8944acd7236385e203c58f0
christopher <hchristopherii27@g.cofc.edu>
2021-09-04 19:43:06 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,16 @@
+"""
+Name: <Christopher Hamilton, II>
+<hello>.py
+
+Problem: This program demonstrates a print statement
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
